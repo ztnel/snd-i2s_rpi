04e1eb85e2a76d36b501576f978db503ace8d880
Huan Truong <htruong@tnhh.net>
2018-03-23 08:29:39 -0400

    Added ability to select rpi gen

--- snd-i2s_rpi.c
+++ snd-i2s_rpi.c
@@ -16,6 +16,7 @@
  * =====================================================================================
  */
 #include <linux/module.h>
+#include <linux/moduleparam.h>
 #include <linux/kernel.h>
 #include <linux/kmod.h>
 #include <linux/platform_device.h>
@@ -38,40 +39,62 @@
  * */
 
 void device_release_callback(struct device *dev) { /*  do nothing */ };
+
+static short rpi_platform_generation = 1;
+module_param(rpi_platform_generation, short, 0);
+MODULE_PARM_DESC(rpi_platform_generation, "Raspberry Pi generation: 0=BCM2835, 1=Others");
+
 static struct asoc_simple_card_info snd_rpi_simple_card_info = {
-.card = "snd_rpi_i2s_card", // -> snd_soc_card.name
-.name = "simple-card_codec_link", // -> snd_soc_dai_link.name
-.codec = "snd-soc-dummy", // "dmic-codec", // -> snd_soc_dai_link.codec_name
-.platform = "3f203000.i2s",
-.daifmt = SND_SOC_DAIFMT_I2S | SND_SOC_DAIFMT_NB_NF | SND_SOC_DAIFMT_CBS_CFS,
-.cpu_dai = {
-.name = "3f203000.i2s", // -> snd_soc_dai_link.cpu_dai_name
-.sysclk = 0 },
-.codec_dai = {
-.name = "snd-soc-dummy-dai", //"dmic-codec", // -> snd_soc_dai_link.codec_dai_name
-.sysclk = 0 },
-};
-static struct platform_device snd_rpi_simple_card_device = {
-.name = "asoc-simple-card", //module alias
-.id = 0,
-.num_resources = 0,
-.dev = { .release = &device_release_callback,
-.platform_data = &snd_rpi_simple_card_info, // *HACK ALERT*
-},
+	.card = "snd_rpi_i2s_card", // -> snd_soc_card.name
+	.name = "simple-card_codec_link", // -> snd_soc_dai_link.name
+	.codec = "snd-soc-dummy", // "dmic-codec", // -> snd_soc_dai_link.codec_name
+	.platform = "3f203000.i2s",
+	.daifmt = SND_SOC_DAIFMT_I2S | SND_SOC_DAIFMT_NB_NF | SND_SOC_DAIFMT_CBS_CFS,
+	.cpu_dai = {
+		.name = "3f203000.i2s", // -> snd_soc_dai_link.cpu_dai_name
+		.sysclk = 0 },
+	.codec_dai = {
+	.name = "snd-soc-dummy-dai", //"dmic-codec", // -> snd_soc_dai_link.codec_dai_name
+	.sysclk = 0 },
 };
 
+static struct platform_device snd_rpi_simple_card_device = {
+	.name = "asoc-simple-card", //module alias
+	.id = 0,
+	.num_resources = 0,
+	.dev = {
+		.release = &device_release_callback,
+		.platform_data = &snd_rpi_simple_card_info, // *HACK ALERT*
+	},
+};
 
 int i2s_rpi_init(void)
 {
 	const char *dmaengine = "bcm2708-dmaengine"; //module name
+	struct platform_device snd_card;
 	int ret;
 
 	printk(KERN_INFO "snd-i2s_rpi: Version %s\n", SND_I2S_RPI_VERSION);
 
 	ret = request_module(dmaengine);
-	//pr_alert("request module load '%s': %d\n",dmaengine, ret);
+	// pr_alert("request module load '%s': %d\n",dmaengine, ret);
 
-	ret = platform_device_register(&snd_rpi_simple_card_device);
+	snd_card = snd_rpi_simple_card_device;
+
+	if (rpi_platform_generation == 0) {
+		struct asoc_simple_card_info card_info_alt;
+
+		card_info_alt = snd_rpi_simple_card_info;
+		card_info_alt.platform = "20203000.i2s";
+		card_info_alt.cpu_dai.name = "20203000.i2s";
+
+		snd_card.dev.platform_data = &card_info_alt;
+	}
+
+	ret = platform_device_register(&snd_card);
+
+
+
 	//pr_alert("register platform device '%s': %d\n",snd_rpi_simple_card_device.name, ret);
 
 	return 0;
